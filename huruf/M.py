8365a76134f216ce805ac0d97b983ee83fc6c83a
ReffSeven <74449710+ReffSeven@users.noreply.github.com>
2020-11-14 16:42:05 +0700

    Update M.py

--- huruf/M.py
+++ huruf/M.py
@@ -7,4 +7,4 @@
 1-----1
 1-----1
 """
-n=int(input('Tinggi M : '));s=int(n/2);print(''.join([''.join([('1' if(i==x and i<s+1) or(i==n-x-1 and i<s) or(x==0) or(x==n-1) else '-') for x in range(n)])+'\n' for i in range(n)]))
+n=int(input('Tinggi M : '));s=int(n/2);print(''.join([''.join([('1' if(i==x and i<s+(1 if(n%2==1) else 0)) or(i==n-x-1 and i<s) or(x==0) or(x==n-1) else '-') for x in range(n)])+'\n' for i in range(n)]))
