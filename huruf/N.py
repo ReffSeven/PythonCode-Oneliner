49c91b2dd0a4bbd79d5a2661f7f5ccf00d1ae065
ReffSeven <74449710+ReffSeven@users.noreply.github.com>
2020-11-14 16:24:30 +0700

    Update N.py

--- huruf/N.py
+++ huruf/N.py
@@ -7,4 +7,4 @@
 1----11
 1-----1
 """
-n=int(input('Tinggi N : '));s=n/2;print(''.join([''.join([('1' if(i==x) or(x==0) or(x==n-1) else '-') for x in range(n)])+'\n' for i in range(n)]))
+n=int(input('Tinggi N : '));print(''.join([''.join([('1' if(i==x) or(x==0) or(x==n-1) else '-') for x in range(n)])+'\n' for i in range(n)]))
